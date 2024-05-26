7d7ad91ef4683fc8eee6939c436dfc212273ac10
rxyhn <rxyhn.dev@gmail.com>
2024-05-26 23:19:19 +0700

    fix: css

--- src/app/components/UI/landing-page.jsx
+++ src/app/components/UI/landing-page.jsx
@@ -8,31 +8,20 @@ const Hero = () => {
 
 export default function LandingPage() {
   return (
-    <div className="relative flex flex-row w-screen h-screen">
-      <div
-        class="L3ft"
-        className="w-[35%] h-screen flex items-center justify-center items-center ml-[50px]"
-      >
-        <div
-          class="box"
-          className="w-full h-[300px] flex justify-center items-center justify-center text-center"
-        >
-          <div class="tagline" className="text-[28px]">
+    <div className="relative flex w-screen h-screen">
+      <div className="w-[35%] h-screen flex items-center justify-center ml-[50px]">
+        <div className="w-full h-[300px] flex justify-center items-center text-center">
+          <div className="text-[28px]">
             <p>
               <span>&quot;</span>Your Complete <span>Sports</span> Destination :{" "}
               <span>Courts</span>, <span>Coaches</span>, and{" "}
-              <span>Competitions</span>
-              <span>&quot;</span>
+              <span>Competitions</span> <span>&quot;</span>
             </p>
           </div>
         </div>
       </div>
-
-      <div
-        class="R1ght"
-        className="w-[65%] h-screen flex justify-center items-center mt-[30px]"
-      >
-        <div class="box" className="flex justify-center items-center">
+      <div className="w-[65%] h-screen flex justify-center items-center mt-[30px]">
+        <div className="flex justify-center items-center">
           <Hero />
         </div>
       </div>
--- src/app/components/UI/navbar.jsx
+++ src/app/components/UI/navbar.jsx
@@ -2,53 +2,45 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 
-const OlahragYukLogo = () => {
+const OlahragaYukLogo = () => {
   return (
     <div className="z-100">
-      <Image
-        alt="logo"
-        width={137}
-        height={85}
-        src="/olahragayuk-logo.png"
-      />
+      <Image alt="logo" width={137} height={85} src="/olahragayuk-logo.png" />
     </div>
   );
 };
 
 const Menu = () => {
   return (
-    <div class="menuNavbar" className="relative flex flex-row gap-[100px] me-[300px] z-100">
-      <a href="">SewaYuk</a>
-      <a href="">TutorYuk</a>
-      <a href="">TurnamenYuk</a>
+    <div className="menuNavbar relative flex gap-10 z-100">
+      <Link href="/sewa-yuk">SewaYuk</Link>
+      <Link href="/tutor-yuk">TutorYuk</Link>
+      <Link href="/turnamen-yuk">TurnamenYuk</Link>
     </div>
   );
 };
 
 const RegistAndLogin = () => {
   return (
-    <div className="flex flex-row gap-[20px] z-100">
-      <div className="h-[40px] w-[115px] bg-white bg-opacity-10 backdrop-blur-sm rounded-[50px] flex items-center justify-center text-center">
+    <div className="flex gap-4 z-100">
+      <div className="h-10 w-28 bg-white bg-opacity-10 backdrop-blur-sm rounded-full flex items-center justify-center text-center">
         <p>Sign In</p>
       </div>
-
-      
-      <div className="h-[40px] w-[115px] bg-[#BEE702] rounded-[50px] flex items-center justify-center text-center text-[#141414]">
-        <p>Sign In</p>
+      <div className="h-10 w-28 bg-[#BEE702] rounded-full flex items-center justify-center text-center text-[#141414]">
+        <p>Sign Up</p>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default function Navbar() {
   return (
-    <div class="navbar" className="fixed z-100 flex w-screen h-[120px] items-center">
-      <div class="boxNavbar" className="flex flex-row w-screen h-[100px] items-center justify-around text-[14px]">
-        <OlahragYukLogo />
+    <nav className="fixed z-100 flex w-screen h-30 items-center bg-transparent">
+      <div className="container mx-auto flex justify-between items-center px-4 md:px-8 lg:px-16">
+        <OlahragaYukLogo />
         <Menu />
         <RegistAndLogin />
       </div>
-    </div>
-  )
+    </nav>
+  );
 }
-
