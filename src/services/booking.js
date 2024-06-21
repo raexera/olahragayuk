15f0c08a6f0e90bef4ed671bad01edbd9c7d5379
rxyhn <rxyhn.dev@gmail.com>
2024-06-21 18:07:43 +0700

    chore: updates

--- src/services/booking.js
+++ src/services/booking.js
@@ -52,3 +52,32 @@ export const getBookings = async (userId) => {
 
   return data;
 };
+
+export const getBookingsByUserId = async (userId) => {
+  const { data, error } = await supabase
+    .from("booking")
+    .select("*")
+    .eq("userid", userId);
+
+  if (error) {
+    console.error("Error fetching bookings:", error);
+    throw error;
+  }
+
+  return data;
+};
+
+export const getBookingById = async (bookingId) => {
+  const { data, error } = await supabase
+    .from("booking")
+    .select("*")
+    .eq("bookingid", bookingId)
+    .single();
+
+  if (error) {
+    console.error(`Error fetching booking with ID ${bookingId}:`, error);
+    throw error;
+  }
+
+  return data;
+};
